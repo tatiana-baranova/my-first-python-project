ef27eba573034792699328f450cecda73dfd4c00
tetiana_baranova <syniovych.tanya@gmail.com>
2026-02-18 12:51:55 +0200

    variable data types

--- chapter_02/task1.py
+++ chapter_02/task1.py
@@ -1,1 +1,15 @@
-print("Hello world")
+number = 5  #int
+print(number)
+number= 7
+info = "value"
+print(info, number)
+
+num = 25.36 #float
+print(num)
+word = "Hello my friend" #string
+print(word)
+boolean = False #bool
+print(boolean)
+
+num2 = 25
+print(num + num2)
