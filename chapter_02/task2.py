3c9802f4f2cc8e018c0994207619374e12c46ec2
tetiana_baranova <syniovych.tanya@gmail.com>
2026-02-18 13:17:30 +0200

    lesson 4, data types

---
+++ chapter_02/task2.py
@@ -0,0 +1,11 @@
+# # Невелика програма
+# a = int(input("Enter num 1: ")) #int
+# b = input("Enter num 2: ") #str
+
+# print(a + int(b))
+
+d = float(input("Enter num 1: "))
+f = input("Enter num 2: ")
+
+d += 2
+print(d + float(f))
