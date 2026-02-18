8c4140bb9c57ed9ab2e05c6690a1fbd2807da48b
tetiana_baranova <syniovych.tanya@gmail.com>
2026-02-18 16:59:36 +0200

    if, else

---
+++ chapter_03/task1.py
@@ -0,0 +1,18 @@
+num = int(input("Enter num: "))
+
+isHasCar = False
+
+if num >= 55 :
+    print("Yes")
+    if num == 100:
+        print("Num si 100")
+elif num == 40:
+    print("Num is 40")
+elif num < 5 :
+    print("Num is less 5")
+else: 
+    print("Nun < 55")
+
+isHappy = True
+if not isHappy:
+    print("Yes, he is happy")
