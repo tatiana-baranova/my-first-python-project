a7b15c6a650cee8e11ddda1999dd06475d2ce933
tetiana_baranova <syniovych.tanya@gmail.com>
2026-02-18 12:23:17 +0200

    lesson 3, print and input

---
+++ chapter_01/task2.py
@@ -0,0 +1,18 @@
+print("Hello", 22.3, 5,)
+print("\n")
+print("Hi friend", 5.5, 5, sep=", ", end=". ")
+print("\n\tHello")
+print("John\'s book")
+
+#Математика
+print(5 + 5) # +, -, *, /
+print(-5.5 % 5)
+print(2 ** 3)
+print(min(5, 11, 25, 36, 2))
+print(max(205, 35, 0, 104))
+print(abs(-3))
+print(pow(5, 6))
+print(round(25.6))
+
+#отримання значення
+input("My name..")
