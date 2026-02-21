6ae082c5d36c952e7a354387c7c2f859458f001d
tetiana_baranova <syniovych.tanya@gmail.com>
2026-02-21 21:38:45 +0200

    lists end loops

--- chapter_04/task5.py
+++ chapter_04/task5.py
@@ -18,3 +18,21 @@
 # print(len(nums)) # довжина списка
 # print(numbers)
 
+# Списки та цикли
+
+# nums = [1, 6, 5, 9, 8, 3, 2]
+# for el in nums:
+#     res = el ** 2
+#     print(res)
+
+
+# user_count_hobby = int(input("Enter hobby number: "))
+
+# i = 0
+# hobby = []
+# while i < user_count_hobby:
+#     text = "Enter hobby" + str(i + 1) + ":"
+#     hobby.append(input(text))
+#     i += 1
+
+# print(hobby)
